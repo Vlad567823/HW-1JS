cf5629c6c6fd3bfee1665426888c653ad4d60ac8
Vlad567823 <165555735+Vlad567823@users.noreply.github.com>
2025-03-30 15:23:34 +0300

    ready

---
+++ base.js
@@ -0,0 +1,21 @@
+const age = "15";
+console.log(age);
+const name = "Vlad"
+console.log(name);
+const  isStudent = "true"
+console.log(isStudent);
+const myString = "Вас делают похожими, чтобы управлять вами. Когда начинается война, вас превращают в патриотов - Жак Фреско."
+console.log(myString);
+let myNumber = 20; // Довільне число
+myNumber += 10;
+console.log(myNumber);
+const myNull = "null"
+console.log(myNull);
+const userName = prompt("Ведіть ваше ім'я");
+alert("Ваше ім'я:" + userName);
+console.log("Тип зміної userName:", userName);
+const userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+console.log("Результат підтвердження:", userAnswer);
+alert("Увага! Дія може бути небезпечною.");
+const isConfirmed = confirm("Ви впевнені, що хочете продовжити?");
+console.log("Користувач підтвердив дію:", isConfirmed);
